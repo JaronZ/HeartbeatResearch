ecafa949374e252be4a2533e909bc912cf1b12aa
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-11-02 14:30:12 +0100

    Initial commit

---
+++ src/main/java/dev/jaronline/Host.java
@@ -0,0 +1,4 @@
+package dev.jaronline;
+
+public class Host {
+}
---
+++ src/main/java/dev/jaronline/Participant.java
@@ -0,0 +1,11 @@
+package dev.jaronline;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the ip address of the person you want to connect to:");
+        Scanner scanner = new Scanner(System.in);
+        String ipAddress = scanner.next();
+    }
+}
