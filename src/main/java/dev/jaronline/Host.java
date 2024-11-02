818d16f38341f7779d87533424bcda4b312681b5
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-11-02 15:47:10 +0100

    feat: add Host and Participant connection

--- src/main/java/dev/jaronline/Host.java
+++ src/main/java/dev/jaronline/Host.java
@@ -1,4 +1,43 @@
 package dev.jaronline;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Host {
+    private static final List<InetSocketAddress> addresses = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/join", new HostHandler(addresses));
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server has started on: " + server.getAddress());
+    }
+
+    static class HostHandler implements HttpHandler {
+        private final List<InetSocketAddress> addresses;
+
+        public HostHandler(List<InetSocketAddress> addresses) {
+            this.addresses = addresses;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            addresses.add(exchange.getLocalAddress());
+            System.out.println(addresses);
+
+            String response = "OK";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 }
--- src/main/java/dev/jaronline/Participant.java
+++ src/main/java/dev/jaronline/Participant.java
@@ -1,11 +1,41 @@
 package dev.jaronline;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Participant {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Enter the ip address of the person you want to connect to:");
         Scanner scanner = new Scanner(System.in);
         String ipAddress = scanner.next();
+        String port = "8080";
+        String path = "join";
+
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create("http://" + ipAddress + ":" + port + "/" + path))
+                    .build();
+            HttpResponse response = httpClient.send(request, new ParticipantHandler());
+            System.out.println(response);
+        }
+
+    }
+
+    static class ParticipantHandler implements HttpResponse.BodyHandler<String> {
+        @Override
+        public HttpResponse.BodySubscriber<String> apply(HttpResponse.ResponseInfo responseInfo) {
+            HttpResponse.BodySubscriber<InputStream> upstream = HttpResponse.BodySubscribers.ofInputStream();
+
+            return HttpResponse.BodySubscribers.mapping(
+                    upstream,
+                    (_) -> "Hey!"
+            );
+        }
     }
 }
