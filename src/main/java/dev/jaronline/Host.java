8b0355e7c5b15bc5f8ead10aefdcaaf8fc0cc90e
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-11-02 16:58:05 +0100

    feat: hearbeat mooi ding

--- src/main/java/dev/jaronline/Host.java
+++ src/main/java/dev/jaronline/Host.java
@@ -7,37 +7,107 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Host {
     private static final List<InetSocketAddress> addresses = new ArrayList<>();
+    private static final Map<InetSocketAddress, HeartbeatTimer> heartbeatTimers = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/join", new HostHandler(addresses));
+        server.createContext("/join", new HostJoinHandler(addresses, heartbeatTimers));
+        server.createContext("/heartbeat", new HostHeartbeatHandler(heartbeatTimers));
         server.setExecutor(null);
         server.start();
         System.out.println("Server has started on: " + server.getAddress());
     }
 
-    static class HostHandler implements HttpHandler {
+    static class HostJoinHandler implements HttpHandler {
         private final List<InetSocketAddress> addresses;
+        private final Map<InetSocketAddress, HeartbeatTimer> heartbeatTimers;
 
-        public HostHandler(List<InetSocketAddress> addresses) {
+        public HostJoinHandler(List<InetSocketAddress> addresses, Map<InetSocketAddress, HeartbeatTimer> heartbeatTimers) {
             this.addresses = addresses;
+            this.heartbeatTimers = heartbeatTimers;
         }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
+            System.out.println("Join received");
             addresses.add(exchange.getLocalAddress());
-            System.out.println(addresses);
+            createResponse(exchange);
+            createHeartbeatTimer(exchange);
+        }
+
+        private void createResponse(HttpExchange exchange) throws IOException {
+            String response = "OK";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+        private void createHeartbeatTimer(HttpExchange exchange) {
+            HeartbeatTimer timer = new HeartbeatTimer();
+            InetSocketAddress address = exchange.getLocalAddress();
+            timer.scheduleHeartbeatTask(() -> {
+                System.out.println("Removing address");
+                heartbeatTimers.remove(address);
+                System.out.println(heartbeatTimers);
+            });
+            heartbeatTimers.put(address, timer);
+        }
+    }
+
+    static class HostHeartbeatHandler implements HttpHandler {
+        private final Map<InetSocketAddress, HeartbeatTimer> heartbeatTimers;
+
+        public HostHeartbeatHandler(Map<InetSocketAddress, HeartbeatTimer> heartbeatTimers) {
+            this.heartbeatTimers = heartbeatTimers;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            System.out.println("Heartbeat received");
+            if (!heartbeatTimers.containsKey(exchange.getLocalAddress())) {
+                String response = "Forbidden";
+                exchange.sendResponseHeaders(403, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+
+            HeartbeatTimer timer = heartbeatTimers.get(exchange.getLocalAddress());
+            timer.restart();
 
             String response = "OK";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
+        }
+    }
+
+    static class HeartbeatTimer extends Timer {
+        private TimerTask task;
+        private Runnable action;
+
+        public void scheduleHeartbeatTask(Runnable action) {
+            this.action = action;
+            this.task = new TimerTask() {
+                @Override
+                public void run() {
+                    action.run();
+                }
+            };
+            schedule(task, 10 * 1000);
+        }
+
+        public void restart() {
+            if (this.task != null && this.action != null) {
+                this.task.cancel();
+                scheduleHeartbeatTask(action);
+            }
         }
     }
 }
--- src/main/java/dev/jaronline/Participant.java
+++ src/main/java/dev/jaronline/Participant.java
@@ -7,6 +7,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Participant {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,15 +17,35 @@ public class Participant {
         String ipAddress = scanner.next();
         String port = "8080";
         String path = "join";
+        String path2 = "heartbeat";
 
-        try (HttpClient httpClient = HttpClient.newHttpClient()) {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create("http://" + ipAddress + ":" + port + "/" + path))
-                    .build();
-            HttpResponse response = httpClient.send(request, new ParticipantHandler());
-            System.out.println(response);
-        }
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://" + ipAddress + ":" + port + "/" + path))
+                .build();
+        HttpResponse response = httpClient.send(request, new ParticipantHandler());
+        String message = (String) response.body();
+        System.out.println(message);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                HttpRequest request1 = HttpRequest.newBuilder()
+                        .GET()
+                        .uri(URI.create("http://" + ipAddress + ":" + port + "/" + path2))
+                        .build();
+                try {
+                    HttpResponse response1 = httpClient.send(request1, new ParticipantHandler());
+                    String message1 = (String) response1.body();
+                    System.out.println(message1);
+                } catch (IOException | InterruptedException exception) {
+                    throw new RuntimeException(exception);
+                }
+
+            }
+        }, 5*1000, 5*1000);
 
     }
 
@@ -32,9 +54,16 @@ public class Participant {
         public HttpResponse.BodySubscriber<String> apply(HttpResponse.ResponseInfo responseInfo) {
             HttpResponse.BodySubscriber<InputStream> upstream = HttpResponse.BodySubscribers.ofInputStream();
 
+            if (responseInfo.statusCode() != 200) {
+                return HttpResponse.BodySubscribers.mapping(
+                        upstream,
+                        _ -> "Nee, ga weg!"
+                );
+            }
+
             return HttpResponse.BodySubscribers.mapping(
                     upstream,
-                    (_) -> "Hey!"
+                    _ -> "Hey!"
             );
         }
     }
